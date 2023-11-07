b444c25968e4611684fc0001b6cd69149796d2d8
alpar <alpar@ALPARSLAN>
2023-11-07 22:07:42 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+ProductManager productMaganer = new ProductManager(new EfProductDal());
+foreach(var product in productMaganer.GetByUnitPrice(50,100))
+{
+    Console.WriteLine(product.ProductName);
+}
