0edae02adcd8d8ef652b216d457ce4df01dd0164
alparslan <alparslan.yuce@icloud.com>
2023-11-08 20:25:53 +0300

    08.11.2023-20:25

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "The product name must consist of at least 2 characters";
+        public static string MaintenanceTime= "System in maintenance";
+        public static string ProductsListed= "Products listed";
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,8 +3,33 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
-ProductManager productMaganer = new ProductManager(new EfProductDal());
-foreach(var product in productMaganer.GetByUnitPrice(50,100))
+ProductTest();
+//CategoryTest();
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    var result = productManager.GetProductDetails();
+
+    if(result.Success==true)
+    {
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        }
+    }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
 }
